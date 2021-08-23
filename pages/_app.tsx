87384de0bb1ffb3d7a638bd71fdd56d6660add9e
paulbtw <p-p-v-2010@hotmail.de>
2021-08-23 10:56:14 +0200

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+import { Provider } from 'next-auth/client';
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+	return (
+		<Provider session={pageProps.session}>
+			<Component {...pageProps} />
+		</Provider>
+	);
+}
+export default MyApp;
